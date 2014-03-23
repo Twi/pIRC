be831b006baea448bff5303dc387b4e771ca9a25
Nicole Brennan <twipony.ts@gmail.com>
2014-03-23 10:21:05 -0700

    Add ability to be made into a python egg

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pIRC',
+      version='0.1',
+      description='A light, customizable python-based IRC bot library.',
+      url='https://github.com/bronyborn/pIRC',
+      author='BronyBorn',
+      author_email='minecraftsolutionsinc@gmail.com',
+      license='Custom',
+      packages=['pIRC'])
